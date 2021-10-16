cb29480971db3e91062e5acd92118860058c8334
Alexandre Soares <alexandresoares@assoares.local>
2021-10-16 14:05:15 -0300

    chore: file update

--- config/database.js
+++ config/database.js
@@ -8,7 +8,7 @@ module.exports = ({ env }) => ({
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', 'strapi_docker'),
+        username: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', `${process.env.DB_PASSWORD}`),
         ssl: env.bool('DATABASE_SSL', false),
       },
